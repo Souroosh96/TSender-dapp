85833620937a14b8301559e0a39f0b977076bcc1
Souroosh <sooroushmemarian@gmail.com>
2025-12-12 20:17:34 -0500

    rainbowKitConfig.tsx updated

--- rainbowKitConfig.tsx
+++ rainbowKitConfig.tsx
@@ -3,7 +3,7 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { mainnet, sepolia, zkSync } from "wagmi/chains";
 
-const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID;
+const projectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID;
 
 if (!projectId) {
     throw new Error("Missing NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID");
