e29f04894f4c1cf558cb7dae21abb51f12a70673
Souroosh <sooroushmemarian@gmail.com>
2025-12-17 23:43:32 -0500

    Adding headers and readme.md file

--- components/ui/InputField.tsx
+++ components/ui/InputField.tsx
@@ -1,3 +1,39 @@
+/**
+ * ------------------------------------------------------------
+ * Project: TSender DApp
+ * File: InputField.tsx
+ * Description: Input field component for the TSender DApp
+ *
+ * Author: Souroosh Memarian
+ * Date: 2025-12-17
+ *
+ * License: MIT
+ * ------------------------------------------------------------
+ * Copyright (c) 2025 Souroosh Memarian
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ * ------------------------------------------------------------
+ */
+
 import React from "react";
 
 interface InputFieldProps {
@@ -7,7 +43,7 @@ interface InputFieldProps {
     type?: string;
     large?: boolean;
     onChange: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
-    className?: string; 
+    className?: string;
 }
 
 export default function InputField({
@@ -17,7 +53,7 @@ export default function InputField({
     type = "text",
     large = false,
     onChange,
-    className, 
+    className,
 }: InputFieldProps) {
     return (
         <div className="flex flex-col space-y-1">
